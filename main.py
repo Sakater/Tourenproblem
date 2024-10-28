e1b530dc66644e75a0588fa99086bdf81cf5a421
Yusuf <yusufsolmaz97@gmail.com>
2024-10-28 15:38:27 +0100

    Routenpunkte werden im FE abgefragt

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import json
 from typing import List
-from fastapi import Request
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from Model import Model
 from Node import Node
 from Tour import Tour
-import Routing as routing
 
 app = FastAPI()
 
@@ -32,18 +30,4 @@ async def create_nodes(nodes: List[Node]):
     model = Model(tour)
     routes = model.execute_model()
     output = {"distances": distances, "routes": routes}
-    print(routing.get_routes(output['routes'], output['distances']))
-    # routesWrapper(routes, distances)
     return output
-
-
-@app.post("/routes")
-async def get_routes(request: Request):
-    try:
-        results = await request.json()
-        routes = routing.get_routes(results['routes'], results['distances'])
-        response = await routing.fetch_routes(routes)
-        response_data = response.json()  # Convert the response to a dictionary
-        return response_data
-    except Exception as e:
-        return {"error": str(e)}
